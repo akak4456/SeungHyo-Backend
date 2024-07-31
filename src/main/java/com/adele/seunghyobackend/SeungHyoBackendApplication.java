83808a99b6607f1161277dc067628814d746bd39
adele <akak4456@naver.com>
2024-07-31 15:58:54 +0900

    [INIT] first commit

---
+++ src/main/java/com/adele/seunghyobackend/SeungHyoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adele.seunghyobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeungHyoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeungHyoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adele/seunghyobackend/SeungHyoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adele.seunghyobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeungHyoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
