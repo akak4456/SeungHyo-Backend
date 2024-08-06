e12ab80f23553938c9dfa093ec68cc6832665825
adele <akak4456@naver.com>
2024-08-06 09:29:44 +0900

    [CNG] spring boot test 가 정상 작동하도록 변경함

--- src/test/java/com/adele/seunghyobackend/SeungHyoBackendApplicationDevTests.java
+++ src/test/java/com/adele/seunghyobackend/SeungHyoBackendApplicationDevTests.java
@@ -9,12 +9,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import static com.adele.seunghyobackend.TestConstant.INTEGRATED_TAG;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestExecutionListeners(listeners = {
+        DotenvTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class
+})
 @ActiveProfiles("dev")
 @Tag(INTEGRATED_TAG)
 @Slf4j
