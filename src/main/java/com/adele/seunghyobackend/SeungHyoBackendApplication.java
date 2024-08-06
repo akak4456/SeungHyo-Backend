414e6e3d05f420386fadd306fb78fa4a29c8f8b7
adele <akak4456@naver.com>
2024-08-06 09:25:43 +0900

    [CNG] docker compose 에서 spring boot application 은 별도 실행하도록 변경함

--- src/main/java/com/adele/seunghyobackend/SeungHyoBackendApplication.java
+++ src/main/java/com/adele/seunghyobackend/SeungHyoBackendApplication.java
@@ -1,5 +1,6 @@
 package com.adele.seunghyobackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SeungHyoBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .load();
+
+        // 시스템 환경 변수로 설정
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(SeungHyoBackendApplication.class, args);
     }
 
