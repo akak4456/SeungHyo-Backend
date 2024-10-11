6bf7a27b7ed8db5e658d2cfc5dd651e32249b53e
akak4456 <akak4456@naver.com>
2024-10-11 16:26:25 +0900

    [CNG] DB 설계함

--- initdb.dev/create.sql
+++ initdb.dev/create.sql
@@ -4,16 +4,16 @@ CREATE DATABASE IF NOT EXISTS `board_service`;
 GRANT ALL PRIVILEGES on *.* TO 'adele'@'%' WITH GRANT OPTION;
 USE member_service;
 CREATE TABLE member (
-    delete_yn VARCHAR(255) DEFAULT 'N',
-    email VARCHAR(255) NOT NULL UNIQUE,
     member_id VARCHAR(255) NOT NULL,
-    member_pw VARCHAR(255) NOT NULL,
-    status_message VARCHAR(255) NOT NULL,
+    member_pw VARCHAR(500) NOT NULL,
+    status_message LONGTEXT NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    delete_yn VARCHAR(10) NOT NULL DEFAULT 'N',
     PRIMARY KEY (member_id)
 );
 CREATE TABLE member_roles (
-    member_id VARCHAR(255) NOT NULL REFERENCES member(member_id),
     roles VARCHAR(255) NOT NULL,
+    member_id VARCHAR(50) NOT NULL REFERENCES member(member_id),
     PRIMARY KEY(member_id, roles)
 );
 USE problem_service;
@@ -23,24 +23,24 @@ CREATE TABLE problem(
     problem_explain LONGTEXT NOT NULL,
     problem_input_explain LONGTEXT NOT NULL,
     problem_output_explain LONGTEXT NOT NULL,
-    IS_GRADABLE VARCHAR(255) NOT NULL DEFAULT('N')
+    IS_GRADABLE VARCHAR(10) NOT NULL DEFAULT('N')
 );
 CREATE TABLE problem_input(
     input_no BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    is_example VARCHAR(255) NOT NULL DEFAULT('N'),
+    is_example VARCHAR(10) NOT NULL DEFAULT('N'),
     problem_no BIGINT NOT NULL REFERENCES problem(problem_no),
     input_source LONGTEXT NOT NULL
 );
 CREATE TABLE problem_output(
     output_no BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    is_example VARCHAR(255) NOT NULL DEFAULT('N'),
+    is_example VARCHAR(10) NOT NULL DEFAULT('N'),
     problem_no BIGINT NOT NULL REFERENCES problem(problem_no),
     output_source LONGTEXT NOT NULL
 );
 CREATE TABLE program_language(
     lang_code VARCHAR(255) NOT NULL,
-    lang_name VARCHAR(255) NOT NULL,
-    is_gradable VARCHAR(255) NOT NULL DEFAULT('N'),
+    lang_name VARCHAR(100) NOT NULL,
+    is_gradable VARCHAR(10) NOT NULL DEFAULT('N'),
     PRIMARY KEY(lang_code)
 );
 CREATE TABLE problem_condition(
@@ -54,7 +54,7 @@ CREATE TABLE submit_list(
     submit_no BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id VARCHAR(255) NOT NULL,
     problem_no BIGINT NOT NULL REFERENCES  problem(problem_no),
-    submit_result VARCHAR(255) NOT NULL,
+    submit_result VARCHAR(50) NOT NULL,
     max_memory DECIMAL(10,2) NOT NULL,
     max_time DECIMAL(10,2) NOT NULL,
     lang_code VARCHAR(255) NOT NULL REFERENCES program_language(lang_code),
@@ -64,11 +64,11 @@ CREATE TABLE submit_list(
 );
 CREATE TABLE problem_grade(
     grade_no BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    grade_result VARCHAR(255) NOT NULL,
+    grade_result VARCHAR(50) NOT NULL,
     input_no BIGINT NOT NULL REFERENCES problem_input(input_no),
     output_no BIGINT NOT NULL REFERENCES problem_output(output_no),
     submit_no BIGINT NOT NULL REFERENCES  submit_list(submit_no),
-    compile_error_reason VARCHAR(255),
+    compile_error_reason VARCHAR(50),
     runtime_error_reason VARCHAR(255),
     grade_case_no BIGINT NOT NULL
 );
@@ -79,8 +79,8 @@ CREATE TABLE problem_program_language_correlation(
 );
 CREATE TABLE problem_tag(
     tag_no BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tag_name VARCHAR(255) NOT NULL,
-    background_color VARCHAR(255) NOT NULL
+    tag_name VARCHAR(500) NOT NULL,
+    background_color VARCHAR(50) NOT NULL
 );
 CREATE TABLE problem_problem_tag_correlation(
     correlation_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -89,7 +89,7 @@ CREATE TABLE problem_problem_tag_correlation(
 );
 CREATE TABLE algorithm_category(
     algorithm_no BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    algorithm_name VARCHAR(255) NOT NULL
+    algorithm_name VARCHAR(500) NOT NULL
 );
 CREATE TABLE problem_algorithm_category_correlation(
     correlation_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -99,8 +99,8 @@ CREATE TABLE problem_algorithm_category_correlation(
 USE board_service;
 CREATE TABLE board_category(
     category_code VARCHAR(255) NOT NULL,
-    category_name VARCHAR(255) NOT NULL,
-    is_for_admin VARCHAR(255) NOT NULL,
+    category_name VARCHAR(500) NOT NULL,
+    is_for_admin VARCHAR(10) NOT NULL,
     order_num INT NOT NULL,
     PRIMARY KEY (category_code)
 );
@@ -110,7 +110,7 @@ CREATE TABLE board(
     board_title VARCHAR(1000) NOT NULL,
     category_code VARCHAR(255) NOT NULL,
     lang_code VARCHAR(255) NOT NULL,
-    lang_name VARCHAR(255) NOT NULL,
+    lang_name VARCHAR(100) NOT NULL,
     like_count BIGINT NOT NULL DEFAULT 0,
     reg_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     problem_no BIGINT NOT NULL,
@@ -122,11 +122,11 @@ CREATE TABLE reply(
     reply_no BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     board_no BIGINT NOT NULL REFERENCES board(board_no),
     member_id VARCHAR(255) NOT NULL,
-    like_count BIGINT NOT NULL,
+    like_count BIGINT NOT NULL DEFAULT 0,
     reply_content LONGTEXT NOT NULL,
     source_code LONGTEXT,
     lang_code VARCHAR(255) NOT NULL DEFAULT 'JAVA_11',
-    lang_name VARCHAR(255) NOT NULL DEFAULT '자바11',
+    lang_name VARCHAR(100) NOT NULL DEFAULT '자바11',
     reg_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
