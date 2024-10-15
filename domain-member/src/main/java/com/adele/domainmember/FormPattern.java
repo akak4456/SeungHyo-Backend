8d479f7477d8a56249e6e4a05ac243c2c3f4077b
akak4456 <akak4456@naver.com>
2024-10-15 14:39:25 +0900

    [CNG] form validation 수정함

--- domain-member/src/main/java/com/adele/domainmember/FormPattern.java
+++ domain-member/src/main/java/com/adele/domainmember/FormPattern.java
@@ -1,8 +1,10 @@
 package com.adele.domainmember;
 
 public class FormPattern {
-    public static final String ID_PATTERN = "(.|\\s)*\\S(.|\\s)*";
-    public static final String PW_PATTERN = "(.|\\s)*\\S(.|\\s)*";
+    // ID는 영어 대소문자와 숫자만 허용하며, 8~16자
+    public static final String ID_PATTERN = "^[a-zA-Z0-9]{8,16}$";
+    // PW는 영어 대소문자, 숫자, 특수문자가 반드시 한 개 이상 포함된 8~16자
+    public static final String PW_PATTERN = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,16}$";
     public static final String STATUS_PATTERN = "(.|\\s)*\\S(.|\\s)*";
     public static final String EMAIL_PATTERN = "(.|\\s)*\\S(.|\\s)*";
 }
